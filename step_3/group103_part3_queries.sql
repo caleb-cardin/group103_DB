6d16a03c1a71464183167a8f69423869682aee9a
Caleb C <caleb.cardin@outlook.com>
2024-02-15 14:15:28 -0800

    Update group103_part3_queries.sql

--- step_3/group103_part3_queries.sql
+++ step_3/group103_part3_queries.sql
@@ -46,13 +46,13 @@ FROM
 JOIN Orders ON Assignments.fkOrderID = Orders.orderID;
  
   -- Create a new assignment
-INSERT INTO Assignments (isComplete, employeeNotes, fkOrderID) VALUES (:is_complete_input, :employee_notes_input, :order_ID_selected_from_create_assignment_from_order_page);
+INSERT INTO Assignments (isComplete, employeeNotes, fkOrderID) VALUES (:is_complete_input, :employee_notes_input, :order_id_selected_from_create_assignment_via_order_page);
 -- Assosiate an order with a Employee
-INSERT INTO AssignmentsHasEmployees (fkAssignmentID, fkEmployeeID) VALUES (:assignment_ID_selected_from_add_assignment_page, :employee_ID_selected_from_add_assignment_page);
+INSERT INTO AssignmentsHasEmployees (fkAssignmentID, fkEmployeeID) VALUES (:assignment_id_selected_from_add_assignment_page, :employee_id_selected_from_add_assignment_page);
 
 -- Update order information
 UPDATE Assignments SET isComplete = :is_complete_input, employeeNotes = :employee_notes_input
-WHERE assignmentID = :assignment_ID_from_update_form,
+WHERE assignmentID = :assignment_id_from_update_form,
 
 
 -- Delete an assignment
@@ -62,7 +62,7 @@ DELETE FROM Assignments USING assignmentID = :assignment_ID_selected_from_browse
 -- Orders ---------------------------------------------------------------------------------------------------
 -- ----------------------------------------------------------------------------------------------------------
  
-  -- Display order information
+  -- Display all order information
 SELECT
     Orders.orderID,
     Orders.orderRequest,
@@ -71,18 +71,28 @@ FROM
 	Orders
 JOIN Assignments ON Orders.orderID = Assignments.fkOrderID;
 
+
+-- Display single Customer's Orders
+SELECT
+    Orders.orderID,
+    Orders.orderRequest,
+    Assignments.isComplete
+FROM
+	Orders
+JOIN Assignments ON Orders.orderID = Assignments.fkOrderID
+JOIN CustomersHasAssignments ON CustomersHasAssignments.CustomerID = :customer_id_selected_from_browse_orders_page;
  
  
  -- Create a new order
 INSERT INTO Orders (orderRequest) VALUES (:request_text_input);
 -- Assosiate an order with a customer
-INSERT INTO CustomersHasOrders (fkCustomerID, fkOrderID) VALUES (:customer_ID_selected_from_add_order_page, :order_ID_selected_from_add_order_page);
+INSERT INTO CustomersHasOrders (fkCustomerID, fkOrderID) VALUES (:customer_id_selected_from_add_order_page, :order_id_selected_from_add_order_page);
 
 -- Update order information
-UPDATE Orders SET orderRequest = :request_text_input WHERE orderID = :order_ID_from_update_form;
+UPDATE Orders SET orderRequest = :request_text_input WHERE orderID = :order_id_from_update_form;
 
 -- Delete a order
-DELETE FROM Orders USING orderID = :order_ID_selected_from_browse_order_page;
+DELETE FROM Orders USING orderID = :order_id_selected_from_browse_order_page;
 
 -- Cleaning up /deleting all orders left hanging from deleted customers
 DELETE FROM Orders WHERE Orders.orderID NOT IN (SELECT CustomersHasOrders.fkOrderID FROM CustomersHasOrders);
@@ -108,10 +118,10 @@ INSERT INTO Customers (customerEmail, customerName) VALUES ( :email_input, :name
 
 -- Update Employee information
 UPDATE Customers SET customerEmail = :email_input, customerName= :name_input, 
-WHERE customerID = :customer_ID_from_update_form;
+WHERE customerID = :customer_id_from_update_form;
 
 -- Delete a Customer
-DELETE FROM Customers USING customerID = :customer_ID_selected_from_browse_customer_page;
+DELETE FROM Customers USING customerID = :customer_id_selected_from_browse_customer_page;
 
 
 
