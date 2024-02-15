0353d519b2f1ddaa5ebec7cd09d19709c63354b0
Caleb C <caleb.cardin@outlook.com>
2024-02-15 12:28:17 -0800

    added DDL

---
+++ step_3/DDL.sql
@@ -0,0 +1,150 @@
+-- CS 340 : Project Step 3 DDL with Sample Data
+-- Group 103: Sharon Bousso, Caleb Cardin
+-- Last edited: 02-15-24
+-- Contains project data definition queries and sample data insert statements
+
+-- -----------------------------------------------------
+-- Table `Orders`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Orders` (
+  orderID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  orderRequest VARCHAR(255) NULL,
+  PRIMARY KEY (orderID),
+  UNIQUE INDEX idorders_UNIQUE (orderID ASC))
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `Assignments`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Assignments` (
+  assignmentID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  isComplete TINYINT NULL DEFAULT 0,
+  employeeNotes VARCHAR(255) NULL,
+  fkOrderID INT UNSIGNED NOT NULL,
+  PRIMARY KEY (assignmentID),
+  CONSTRAINT fk_Assignments_Orders1
+    FOREIGN KEY (fkOrderID)
+    REFERENCES `Orders` (orderID)
+    ON DELETE CASCADE)
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `Customers`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Customers` (
+  customerID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  customerEmail VARCHAR(255) NOT NULL,
+  customerName VARCHAR(255) NOT NULL,
+  PRIMARY KEY (customerID),
+  UNIQUE INDEX idcustomers_UNIQUE (customerID ASC))
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `Employees`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Employees` (
+  employeeID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  employeeEmail VARCHAR(255) NOT NULL,
+  employeeName VARCHAR(255) NOT NULL,
+  PRIMARY KEY (employeeID),
+  UNIQUE INDEX idemployees_UNIQUE (employeeID ASC))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `Customers_has_Orders`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `CustomersHasOrders` (
+  customerOrderID INT UNSIGNED NOT NULL,
+  fkCustomerID INT UNSIGNED,
+  fkOrderID INT UNSIGNED NOT NULL,
+  PRIMARY KEY (customerOrderID),
+  INDEX fk_customers_has_orders_orders1_idx (fkOrderID ASC),
+  INDEX fk_customers_has_orders_customers1_idx (fkCustomerID ASC),
+  CONSTRAINT fk_customers_has_orders_customers1
+    FOREIGN KEY (fkCustomerID)
+    REFERENCES `Customers` (customerID)
+    ON DELETE SET NULL
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_customers_has_orders_orders1
+    FOREIGN KEY (fkOrderID)
+    REFERENCES `Orders` (orderID)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `AssignmentsHasEmployees`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `AssignmentsHasEmployees` (
+  assignmentEmployeeID INT UNSIGNED NOT NULL,
+  fkAssignmentID INT UNSIGNED NOT NULL,
+  fkEmployeeID INT UNSIGNED NOT NULL,
+  PRIMARY KEY (assignmentEmployeeID),
+  INDEX fk_assignments_has_employees_employees1_idx (fkEmployeeID ASC),
+  INDEX fk_assignments_has_employees_assignments1_idx (fkAssignmentID ASC),
+  CONSTRAINT fk_assignments_has_employees_assignments1
+    FOREIGN KEY (fkAssignmentID)
+    REFERENCES `Assignments` (assignmentID)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_assignments_has_employees_employees1
+    FOREIGN KEY (fkEmployeeID)
+    REFERENCES `Employees` (employeeID)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Inserts
+-- -----------------------------------------------------
+
+-- Insert data into the Customers table
+INSERT INTO Customers (customerID, customerEmail, customerName)
+VALUES
+    (1, 'bigdata@data.data', 'BigData Co.'),
+    (2, 'localfarmsMGMT@farms.com', 'LocalFarmersDB'),
+    (3, 'sandyapples@apples.com', 'Sandy Arthur\'s Apples'),
+    (4, 'john_frill@hello.com', 'Johnny Frillkist');
+    
+-- Insert data into the Employees table
+INSERT INTO Employees (employeeID, employeeEmail, employeeName)
+VALUES
+    (1, 'joeF@dbRus.com', 'Joe Friday'),
+    (2, 'maryA@dbRus.com', 'Mary Alberteen'),
+    (3, 'carolS@dbRus.com', 'Carol Smith'),
+    (4, 'daveV@dbRus.com', 'Dave Vermation');
+
+-- Insert data into the Orders table
+INSERT INTO Orders (orderID, orderRequest)
+VALUES
+    (1, 'Need to update/ add to stock types on existing DB'),
+    (2, 'Dropped entire DB - need help!'),
+    (3, 'Need new query to find all apples of a particular variety and weight'),
+    (4, 'Need to create a new DBMS for my company');
+    
+    -- Insert data into the Assignments table
+INSERT INTO Assignments (assignmentID, isComplete, employeeNotes, fkOrderID)
+VALUES
+    (1, TRUE, '1 - Started work on mm-dd-yy, 2 - Mary joined assignment, 3 - Completed mm-dd-yy', 1),
+    (2, FALSE, '1 - Carol and Mary started work dd-mm-yy', 3),
+    (3, FALSE, '1 - Dave started mm-dd-yy', 4);
+
+-- Insert data into the Customers_has_Orders table
+INSERT INTO CustomersHasOrders (customerOrderID, fkCustomerID, fkOrderID)
+VALUES
+    (1, 2, 1),
+    (2, 1, 2),
+    (3, 3, 3),
+    (4, 2, 3),
+    (5, 4, 4);
+    
+-- Insert data into the Assignments_has_Employees table
+INSERT INTO AssignmentsHasEmployees (assignmentEmployeeID, fkAssignmentID, fkEmployeeID)
+VALUES
+    (1, 1, 1),
+    (2, 1, 2),
+    (3, 2, 2),
+    (4, 2, 3),
+    (5, 3, 4);
--- step_3/group103_part3_queries.sql
+++ step_3/group103_part3_queries.sql
@@ -1,1 +1,114 @@
- -- Database Services Database Data Queries -- Authors: Sharon Bousso, Caleb Cardin -- Created 02.15.2024  -- Last Edited 02.15.2024
+-- Database Services Database Data Queries -- Authors: Sharon Bousso, Caleb Cardin -- Created 02.15.2024  -- Last Edited 02.15.2024
+-- Citation for the following code snippet:
+-- Date(mm/dd/YYYY): 02/15/2024 
+-- Adapted from: Project Step 3 Draft Version: Design HTML Interface + DML SQL (Canvas Module 6)
+-- Source URL: https://canvas.oregonstate.edu/courses/1946034/assignments/9456216 
+ 
+-- Employees  -----------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------------------------------------
+-- Show all employees with their repective number of assignments 
+SELECT
+    Employees.employeeName,
+    Employees.employeeEmail,
+    COUNT(AssignmentsHasEmployees.assignmentEmployeeID) as `Assignment Count`
+FROM
+    Employees
+LEFT JOIN AssignmentsHasEmployees ON AssignmentsHasEmployees.fkEmployeeID = Employees.employeeID
+GROUP BY
+    Employees.employeeName,
+    Employees.employeeEmail
+DESC;
+
+-- Create a new employee
+INSERT INTO Employees (employeeEmail, employeeName) VALUES ( :emailInput, :nameInput);
+
+-- Update Employee information
+UPDATE Employees SET employeeEmail = :emailInput, lname= :nameInput, 
+WHERE employeeID = :employee_ID_from_update_form;
+
+-- Delete an Employee
+DELETE FROM Employees USING employeeID = :employee_ID_selected_from_browse_employee_page;
+
+
+
+-- Assignments ----------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------------------------------------
+ 
+ -- Display assignment information along with respective order
+SELECT
+	Assignments.assignmentId,
+    Assignments.isComplete,
+    Assignments.employeeNotes,
+    Orders.orderID,
+    Orders.orderRequest
+FROM
+    Assignments
+JOIN Orders ON Assignments.fkOrderID = Orders.orderID;
+ 
+  -- Create a new assignment
+INSERT INTO Assignments (isComplete, employeeNotes, fkOrderID) VALUES (:is_complete_input, :employee_notes_input, :order_ID_selected_from_create_assignment_from_order_page);
+-- Assosiate an order with a Employee
+INSERT INTO AssignmentsHasEmployees (fkAssignmentID, fkEmployeeID) VALUES (:assignment_ID_selected_from_add_assignment_page, :employee_ID_selected_from_add_assignment_page);
+
+-- Update order information
+UPDATE Assignments SET isComplete = :is_complete_input, employeeNotes = :employee_notes_input
+WHERE assignmentID = :assignment_ID_from_update_form,
+
+
+-- Delete an assignment
+DELETE FROM Assignments USING assignmentID = :assignment_ID_selected_from_browse_assignment_page;
+
+
+-- Orders ---------------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------------------------------------
+ 
+  -- Display order information
+SELECT
+    Orders.orderID,
+    Orders.orderRequest,
+    Assignments.isComplete
+FROM
+	Orders
+JOIN Assignments ON Orders.orderID = Assignments.fkOrderID;
+
+ 
+ 
+ -- Create a new order
+INSERT INTO Orders (orderRequest) VALUES (:request_text_input);
+-- Assosiate an order with a customer
+INSERT INTO CustomersHasOrders (fkCustomerID, fkOrderID) VALUES (:customer_ID_selected_from_add_order_page, :order_ID_selected_from_add_order_page);
+
+-- Update order information
+UPDATE Orders SET orderRequest = :request_text_input WHERE orderID = :order_ID_from_update_form;
+
+-- Delete a Customer
+DELETE FROM Orders USING orderID = :order_ID_selected_from_browse_order_page;
+
+-- Customers ------------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------------------------------------
+ 
+-- Show all customers with their repective number of assignments 
+SELECT
+    Customers.employeeName,
+    Customers.employeeEmail,
+    COUNT(CustomersHasOrders.assignmentOrderID) as `Order Count`
+FROM
+    Customers
+LEFT JOIN CustomersHasOrders ON CustomersHasOrders.fkCustomerID = Customers.customerID
+GROUP BY
+    Customers.customerName,
+    Customers.customerEmail
+DESC;
+
+-- Create a new customer
+INSERT INTO Customers (customerEmail, customerName) VALUES ( :email_input, :name_input);
+
+-- Update Employee information
+UPDATE Customers SET customerEmail = :email_input, customerName= :name_input, 
+WHERE customerID = :customer_ID_from_update_form;
+
+-- Delete a Customer
+DELETE FROM Customers USING customerID = :customer_ID_selected_from_browse_customer_page;
+
+
+
