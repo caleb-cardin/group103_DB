a307ee69efc9849bfc5fed6e2f907c9e05014ebb
Caleb C <caleb.cardin@outlook.com>
2024-02-29 08:32:18 -0800

    added Node, Handlebars, etc
    Working : Employee Add / Display
    Not Working: Employee Delete
    Not yet implemented: Update
    
    Currently follows https://github.com/osu-cs340-ecampus/nodejs-starter-app/tree/main/ up to step 7. not including step 6 (may need to add dropdown search for assignments, for example). (This does not include the node-modules folder, see github steps)
    Currently does not implement HTML pages from step 3, including index, nav, etc
    
    added Node, Handlebars, etc
    
    Working : Employee Add / Display
    Not Working: Employee Delete
    Not yet implemented: Update
    
    Currently follows https://github.com/osu-cs340-ecampus/nodejs-starter-app/tree/main/ up to step 7. not including step 6 (may need to add dropdown search for assignments, for example). (This does not include the node-modules folder, see github steps)
    Currently does not implement HTML pages from step 3, including index, nav, etc

---
+++ step_4/App.js
@@ -0,0 +1,88 @@
+// https://github.com/osu-cs340-ecampus/nodejs-starter-app/tree/main/Step%200%20-%20Setting%20Up%20Node.js
+
+
+// App.js
+
+/*
+    SETUP
+*/
+var express = require('express');   // We are using the express library for the web server
+var app = express();            // We need to instantiate an express object to interact with the server in our code
+PORT = 13256;                 // Set a port number at the top so it's easy to change in the future
+var db = require('./database/db-connector');
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');     // Import express-handlebars
+app.engine('.hbs', engine({ extname: ".hbs" }));  // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+/*
+    ROUTES
+*/
+
+app.get('/', function (req, res) {
+
+    let selectEmployeeQuery = `
+        SELECT
+            Employees.employeeID,
+            Employees.employeeName,
+            Employees.employeeEmail,
+            COUNT(AssignmentsHasEmployees.assignmentEmployeeID) as \`assignmentCount\`
+        FROM
+            Employees
+        LEFT JOIN AssignmentsHasEmployees ON AssignmentsHasEmployees.fkEmployeeID = Employees.employeeID
+        GROUP BY
+            Employees.employeeID
+        ASC;
+        `;
+    db.pool.query(selectEmployeeQuery, function (error, rows, fields) {    // Execute the query
+
+        res.render('employee', { data: rows });                  // Render the index.hbs file, and also send the renderer
+    })
+});                                        // requesting the web site.
+app.post('/add-employee-form', function (req, res) {
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Employees (employeeEmail, employeeName) VALUES ('${data['input-email']}', '${data['input-name']}')`;
+    db.pool.query(query1, function (error, rows, fields) {
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+
+        // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
+        // presents it on the screen
+        else {
+            res.redirect('/');
+        }
+    })
+});
+app.delete('/delete-employee-ajax', function(req,res,next){
+    let data = req.body;
+    let employeeID = parseInt(data.id);
+    let delete_employee = `DELETE FROM Employees USING Employees.employeeID = ${employeeID}`;
+
+        // Run the 1st query
+        db.pool.query(delete_employee, [employeeID], function(error, rows, fields) {
+
+        if (error) {
+            console.log(error);
+            res.sendStatus(400);
+        } else {
+            res.sendStatus(204);
+        }
+    })
+  });
+/*
+    LISTENER
+*/
+app.listen(PORT, function () {            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
+    console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
+});
---
+++ step_4/js/add_employee.js
@@ -0,0 +1,92 @@
+let addEmployeeForm = document.getElementById('add-employee-form');
+
+// Modify the objects we need
+addEmployeeForm.addEventListener("submit", function (e) {
+    
+    // Prevent the form from submitting
+    e.preventDefault();
+
+    // Get form fields we need to get data from
+    let inputEmail = document.getElementById("input-email");
+    let inputName = document.getElementById("input-name");
+
+    // Get the values from the form fields
+    let emailValue = inputEmail.value;
+    let nameValue = inputName.value;
+
+    // Put our data we want to send in a javascript object
+    let data = {
+        employeeEmail: emailValue,
+        employeeName: nameValue,
+    }
+    
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/add-employee-ajax", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+            // Add the new data to the table
+            addRowToTable(xhttp.response);
+
+            // Clear the input fields for another transaction
+            inputName.value = '';
+            inputEmail.value = '';
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 200) {
+            console.log("There was an error with the input.")
+        }
+    }
+
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+
+})
+
+
+// Creates a single row from an Object representing a single record from 
+// bsg_people
+addRowToTable = (data) => {
+
+    // Get a reference to the current table on the page and clear it out.
+    let currentTable = document.getElementById("employee-table");
+
+    // Get the location where we should insert the new row (end of table)
+    let newRowIndex = currentTable.rows.length;
+
+    // Get a reference to the new row from the database query (last object)
+    let parsedData = JSON.parse(data);
+    let newRow = parsedData[parsedData.length - 1]
+
+    // Create a row and 4 cells
+    let row = document.createElement("TR");
+    let idCell = document.createElement("TD");
+    let nameCell = document.createElement("TD");
+    let emailCell = document.createElement("TD");
+    let deleteCell = document.createElement("TD");
+
+    // Fill the cells with correct data
+    idCell.innerText = newRow.id;
+    nameCell.innerText = newRow.name;
+    emailCell.innerText = newRow.email;
+    deleteCell = document.createElement("button");
+    deleteCell.innerHTML = "Delete";
+    deleteCell.onclick = function(){
+        deleteEmployee(newRow.id);
+    };
+
+
+    // Add the cells to the row 
+    row.appendChild(idCell);
+    row.appendChild(nameCell);
+    row.appendChild(emailCell);
+    row.appendChild(deleteCell);
+
+    row.setAttribute('data-value', newRow.id);
+
+    // Add the row to the table
+    currentTable.appendChild(row);
+}
