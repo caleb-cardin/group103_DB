47b5368aac77eb20d61d12d78e1484fdae4418b0
Sharon Bousso <boussos@flip4.engr.oregonstate.edu>
2024-03-08 18:16:23 -0800

    Added css styling

--- step_5/App.js
+++ step_5/App.js
@@ -15,6 +15,7 @@ app.set('view engine', '.hbs');
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
+//app.use(express.static(path.join(__dirname, '/public')));
 /*
     ROUTES
 */
